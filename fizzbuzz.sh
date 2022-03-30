a86ee1503b79bb809bbce2fe8c803c3289a7042d
SteveBurgos95 <burgossteve95@gmail.com>
2022-03-30 14:36:15 -0500

    m

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -1,16 +1,19 @@
-
 read -p "Your Number " number
 
 if test $number -gt 0 && test $number -lt 21
 then
    
-    if test $((number % 5)) -eq 0 || test $((number % 3)) -eq 0
+    if test $((number % 5)) -eq 0 && test $((number % 3)) -eq 0
     then
         printf fizzbuzz
     else
         if test $((number % 5)) -eq 0
         then
           printf buzz
+        fi
+        if test $((number % 3)) -eq 0
+        then
+          printf fizz
         fi
     fi
 
