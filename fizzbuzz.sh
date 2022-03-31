e022a177e0b14222e1c32135b574846bb3d3ab17
SteveBurgos95 <burgossteve95@gmail.com>
2022-03-30 22:08:04 -0500

    finish

--- fizzbuzz.sh
+++ fizzbuzz.sh
@@ -3,18 +3,13 @@ read -p "Your Number " number
 if test $number -gt 0 && test $number -lt 21
 then
    
-    if test $((number % 5)) -eq 0 && test $((number % 3)) -eq 0
+    if test $((number % 3)) -eq 0
     then
-        printf fizzbuzz
-    else
-        if test $((number % 5)) -eq 0
-        then
-          printf buzz
-        fi
-        if test $((number % 3)) -eq 0
-        then
-          printf fizz
-        fi
+        printf fizz
     fi
+    if test $((number % 5)) -eq 0
+    then
+        printf buzz
+     fi
 
 fi
